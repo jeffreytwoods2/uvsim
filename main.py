4b8a39275d9eb398708ec52c4bc99646d4ebb479
Ryan Coutts <ryan.coutts98@gmail.com>
2024-06-05 14:02:28 -0600

    Add function to get and validate input file's path

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from vm import VM
+import os
+
+def get_file_path():
+    print("Please enter the path to your input file:")
+    file_path = input("> ")
+    if not os.path.exists(file_path):
+        raise ValueError(f"File path {file_path} was not found.")
+    
+    return file_path
 
 def main():
+    file_path = get_file_path()
     vm = VM()
-    vm.load_program("test_files/Test2.txt")
+    vm.load_program(file_path)
     vm.run()
     print("\nFINAL STATE")
     print(vm)
