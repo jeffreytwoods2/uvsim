bf35db90e36e5a80e86f803ef19c89233ddc5d13
Ryan Coutts <ryan.coutts98@gmail.com>
2024-06-19 09:28:07 -0600

    Update main.py to run with the gui

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-from classes import VM, ProgramLoader
-import os
-
-def get_file_path():
-    file_path = input("Please provide a file to run:\n> ")
-
-    while not os.path.exists(file_path):
-        file_path = input("File not found. Please provide a valid file path:\n> ")
-    
-    return file_path
+import gui
+import tkinter as tk
 
 def main():
-    file_path = get_file_path()
-    vm = VM()
-    pl = ProgramLoader()
-    pl.load(vm, file_path)
-    vm.run()
+    root = tk.Tk()
+    gui.VMApp(root)
+    root.mainloop()
 
 main()
